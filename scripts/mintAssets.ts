15fdfbf22efee1f87c4db357702c1ab2052c395b
Alex Connolly <alexconnollyprivate@gmail.com>
2023-12-13 18:18:47 +1100

    Update mintAssets.ts

--- scripts/mintAssets.ts
+++ scripts/mintAssets.ts
@@ -28,8 +28,8 @@ const mint = async (provider: Provider): Promise<TransactionResponse> => {
   // The network has introduced a minimum gas price of 100 Gwei to protect it against SPAM traffic, ensure it can process transactions efficiently and remain cost-effective. 
   // Transactions with a tip cap below 100 Gwei are rejected by the RPC. This limit ensures a standard for transaction costs. This also implies that the fee cap must be greater than or equal to 100 Gwei.
   const gasOverrides = {
-    maxPriorityFeePerGas: 100e9, // 100 Gwei
-    maxFeePerGas: 150e9,
+    maxPriorityFeePerGas: 10e9, // 100 Gwei
+    maxFeePerGas: 15e9,
     gasLimit: 200000, // Set an appropriate gas limit for your transaction
   };
   
